dde2e2ac727751414c7fb880aee767b78b53d152
barrentr <115055463+barrentr@users.noreply.github.com>
2022-10-17 21:06:26 -0700

    Delete data_cleaning.py

--- data_cleaning.py
+++
@@ -1,4 +0,0 @@
-df['Timestamp']=df.index
-df['Month'] = pd.DatetimeIndex(df['Timestamp']).month
-df['Year'] = pd.DatetimeIndex(df['Timestamp']).year
-df = df.reindex(columns=['Timestamp','Year','Month','Fremont Bridge Total'])
