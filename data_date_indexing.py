5bc8d5f7f7f34617084d81336f1ce3e3cb6ff350
barrentr <115055463+barrentr@users.noreply.github.com>
2022-10-17 20:59:55 -0700

    Update data_date_indexing.py

--- data_date_indexing.py
+++ data_date_indexing.py
@@ -4,4 +4,5 @@ df.index=pd.to_datetime(df.index)
 # Summarize data on daily, monthly, yearly bases
 df_daily = df.resample('D').sum()
 df_monthly = df.resample('M').sum()
+df_quarterly = df.resample('Q').sum()
 df_yearly = df.resample('Y').sum()
