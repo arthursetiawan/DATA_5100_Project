534f3047efe14a9b40d47f6eae7108a9b05bf7b4
barrentr <115055463+barrentr@users.noreply.github.com>
2022-10-17 17:10:08 -0700

    Update data_upload.py

--- data_upload.py
+++ data_upload.py
@@ -1,10 +1,5 @@
 # Import pandas, numpy, and matplotlib
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
-# seaborn is a data visualization library built on matplotlib
-import seaborn as sns 
-
-# Download
+# Download dataset
 df = pd.read_csv('https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD')
