6f0196b4d952434f40ce322b0acbbd2cb8c62402
barrentr <115055463+barrentr@users.noreply.github.com>
2022-10-23 11:16:00 -0700

    Update data_date_indexing.py

--- data_date_indexing.py
+++ data_date_indexing.py
@@ -1,5 +1,24 @@
+# Colab notebook here: https://colab.research.google.com/drive/118AF1WzMgXpQbdc_xiPFRMEBpmeMM9bc?usp=sharing
+
+## Run code from data_upload.py
+
+# Import pandas, numpy
+import pandas as pd
+import numpy as np
+
+# Download dataset
+df = pd.read_csv('https://github.com/arthursetiawan/DATA_5100_Project/blob/3ef28a95388bcf920158a93f95f13e64e68d1bfc/Fremont_Bridge_Bicycle_Counter.csv?raw=true',index_col=0)
+
+# Remove unnecessary columns
+df = df.drop(columns=['Fremont Bridge East Sidewalk','Fremont Bridge West Sidewalk'])
+
 # Change index to DatetimeIndex
 df.index=pd.to_datetime(df.index)
+
+# Replace empty data with quadratic interpolated data
+df['Fremont Bridge Total'] = df['Fremont Bridge Total'].interpolate(method='quadratic')
+
+## End code from data_upload.py
 
 # Summarize data on daily, monthly, yearly bases
 df_daily = df.resample('D').sum()
